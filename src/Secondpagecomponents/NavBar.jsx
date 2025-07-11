def3ac5c857d1efc6ca9d06ccc64f2a07ccbb0c1
nduking <114291880+nduking@users.noreply.github.com>
2025-07-11 16:40:22 +0100

    svg images added

--- src/Secondpagecomponents/NavBar.jsx
+++ src/Secondpagecomponents/NavBar.jsx
@@ -4,25 +4,36 @@ import Button from "../components/mini-component/Button";
 
 const NavBar = () => {
   return (
-    <nav className="h-max w-max bg-brainyWavePurple">
-      <div>
-        <Link to="/">Brainwave.io</Link>
+   <nav className="flex items-center justify-between px-12 py-10 h-11 flex-cols">
+      <div className="font-rubik">
+        <Link to="/" className="text-2xl font-bold">
+          Brainwave.io
+        </Link>
       </div>
-      <ul>
-        <Link to="/">Demos</Link>
-        <Link to="/">Pages</Link>
-        <Link to="/">Support</Link>
-        <Link to="/">Contact</Link>
-      </ul>
-
-      <div>
-        <Button
-          className={`text-[15px] text-brainyWaveBlack font-bold`}
-          btnText={"Login"}
+      <div className="flex items-center justify-between space-x-6 flex-cols">
+        <ul className="flex gap-2 space-x-5">
+          <Link to="/" className="text-[#161C2D] font-bold">
+            Demos
+          </Link>
+          <Link to="/" className="text-[#161C2D] font-bold">
+            Pages
+          </Link>
+          <Link to="/" className="text-[#161C2D] font-bold">
+            Support
+          </Link>
+          <Link to="/" className="text-[#161C2D] font-bold">
+            Contact
+          </Link>
+        </ul>
+      </div>
+      <div className="flex items-center justify-between gap-3">
+         <Button
+          className={`py-2 px-4 rounded-lg text-brainyWaveBlack font-bold text-[15px]`}
+          btnText={"Get A Free Quote"}
         />
-        <Button
-          className={`text-[15px] text-brainyWaveBlack font-bold`}
-          btnText={"Sign up"}
+         <Button
+          className={`py-2 px-4 rounded-lg text-brainyWaveBlack font-bold text-[15px]`}
+          btnText={"Get A Free Quote"}
         />
       </div>
     </nav>
